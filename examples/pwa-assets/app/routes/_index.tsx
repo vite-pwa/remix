3ba5e4abe03efe39405d6d1f505474b72205458e
userquin <userquin@gmail.com>
2024-03-23 01:30:33 +0100

    chore: change example title

--- examples/pwa-assets/app/routes/_index.tsx
+++ examples/pwa-assets/app/routes/_index.tsx
@@ -2,8 +2,8 @@ import type { MetaFunction } from '@remix-run/node'
 
 export const meta: MetaFunction = () => {
   return [
-    { title: 'New Remix App' },
-    { name: 'description', content: 'Welcome to Remix!' },
+    { title: 'Remix PWA App' },
+    { name: 'description', content: 'Welcome to Remix PWA!' },
   ]
 }
 
