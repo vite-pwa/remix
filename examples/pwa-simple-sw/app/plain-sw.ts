d7fd757f60972d85230f78ed5eff56770f5ab3c6
Joaquín Sánchez <userquin@gmail.com>
2024-05-14 22:14:20 +0200

    chore: update pnpm to 9.1.1 (#7)
    * chore: rebuild lock file
    
    * chore: fix examples
    
    * chore: update to pnpm 9.1.1

--- examples/pwa-simple-sw/app/plain-sw.ts
+++ examples/pwa-simple-sw/app/plain-sw.ts
@@ -3,25 +3,45 @@
 import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
 import { clientsClaim } from 'workbox-core'
 import { NavigationRoute, registerRoute } from 'workbox-routing'
+import { navigateFallback, ssr } from 'virtual:vite-pwa/remix/sw'
 import { setupRoutes } from './shared-sw'
 
 declare let self: ServiceWorkerGlobalScope
 
-// self.__WB_MANIFEST is default injection point
+const url = navigateFallback ?? '/'
+
+/// self.__WB_MANIFEST is the default injection point
 const manifest = self.__WB_MANIFEST
 if (import.meta.env.DEV) {
+  const entry = manifest.findIndex(entry => typeof entry !== 'string' && entry.url === url)
+  if (entry !== -1)
+    manifest.splice(entry, 1)
+
   // add the navigateFallback to the manifest
-  manifest.push({ url: '/', revision: Math.random().toString() })
+  manifest.push({ url, revision: Math.random().toString() })
 }
+
 precacheAndRoute(manifest)
 
 // clean old assets
 cleanupOutdatedCaches()
 
+let allowlist: RegExp[] | undefined
+// in dev mode, we disable precaching to avoid caching issues
+if (import.meta.env.DEV) {
+  if (ssr) {
+    // add the navigateFallback to the manifest
+    allowlist = [new RegExp(`^${url}$`)]
+  }
+  else {
+    allowlist = [/^index.html$/]
+  }
+}
+
 // to allow work offline
 registerRoute(new NavigationRoute(
-  createHandlerBoundToURL('/'),
-  { allowlist: [/^\/$/] },
+  createHandlerBoundToURL(ssr ? url : 'index.html'),
+  { allowlist },
 ))
 
 setupRoutes()
